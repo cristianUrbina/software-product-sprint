3a195e8ec0df74f245aeb522c75e9c8a68dc3451
Cristian Urbina <A01412249@itesm.mx>
2021-06-19 16:33:36 +0000

    Add a map in a new section

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -35,3 +35,10 @@ async function getRandomFact(){
     const randomFactContainer = document.getElementById('random-fact-container');
     randomFactContainer.innerText = fact;
 }
+
+/** Creates a map and adds it to the page. */
+function createMap() {
+  const map = new google.maps.Map(
+      document.getElementById('map'),
+      {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+}
