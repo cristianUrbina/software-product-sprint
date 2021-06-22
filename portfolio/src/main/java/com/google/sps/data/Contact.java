54a1467726c6829e9b877c32ce30396484dfb1a0
Cristian Urbina <A01412249@itesm.mx>
2021-06-22 18:41:43 +0000

    Add contact class

---
+++ portfolio/src/main/java/com/google/sps/data/Contact.java
@@ -0,0 +1,11 @@
+package com.google.sps.data;
+
+public final class Contact{
+    private final String first_name;
+    private final String last_name;
+
+    public Contact(String first_name, String last_name){
+        this.first_name = first_name;
+        this.last_name = last_name;
+    }
+}
